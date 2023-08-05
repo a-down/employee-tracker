217a65dc805851f005e4db803300d8b74f8a6090
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-05 09:12:10 -0500

    initial sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,31 @@ CREATE DATABASE employees;
 
 USE employees;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) UNIQUE NOT NULL
 );
+
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
+)
+
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL,
+)
+
+SELECT * FROM departments,
+SELECT * FROM roles,
+SELECT * FROM employees,
+
 
