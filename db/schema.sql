1d77bd02c64c4bac7bab0d31bf4f674544874618
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-05 09:24:32 -0500

    started seed data

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE employees;
+USE employees_db;
 
 CREATE TABLE departments (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) UNIQUE NOT NULL
 );
 
@@ -13,8 +13,8 @@ CREATE TABLE roles (
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
-)
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
 
 CREATE TABLE employees (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -23,11 +23,25 @@ CREATE TABLE employees (
   role_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
   manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL,
-)
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
 
-SELECT * FROM departments,
-SELECT * FROM roles,
-SELECT * FROM employees,
+INSERT INTO department (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
+INSERT INTO roles (title, salary, department_id)
+VALUES
+  ('Customer Rep', '80000', '1'),
+  ('Senior Developer', '120000', '2'),
+  ('Accountant', '75000', '3'),
+  ('Legal Advisor', '130000', '4');
 
+INSERT INTO
+
+SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
