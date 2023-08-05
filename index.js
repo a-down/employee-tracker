5b008c3b9e68fd092fe6525bb14fb755318221ea
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-05 13:54:47 -0500

    add department feature is active

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const connection = require("./config/connection");
-const { listAllDepartments, listAllRoles, listAllEmployees } = require("./lib/queries")
+const { listAllDepartments, listAllRoles, listAllEmployees, addDepartment } = require("./lib/queries")
 const { displayTable } = require("./lib/displays")
 /*
   There are a lot of menu items presented to users in this app. The only real way you cam manage them 
@@ -17,36 +17,54 @@ function start(){
       message: "Choose an item from the list below:",
       name: "option", 
       choices: [
-        "List All Departments",
-        "List All Roles",
-        "List All Employees"
+        "List all Departments",
+        "List all Roles",
+        "List all Employees",
+        "Add a Department",
+        "Add a Role",
+        "Add an Employee",
+        "Update Employee Role",
       ]
     }
   ]).then( response => {
     switch(response.option){
-      case "List All Departments":
+      case "List all Departments":
         listAllDepartments().then(([rows]) => {
           displayTable(rows);
           start();
         });
         break;
 
-      case "List All Roles":
+      case "List all Roles":
         listAllRoles().then(([rows]) => {
           displayTable(rows);
           start();
         });
         break;
       
-      case "List All Employees":
+      case "List all Employees":
         listAllEmployees().then(([rows]) => {
           displayTable(rows);
           start();
-        })
-
-
-      default:
-        start();
+        });
+        break;
+      
+      case "Add a Department":
+        inquirer.prompt([
+          {
+            type: 'input',
+            message: 'What is the name of the new department?',
+            name: 'departmentName'
+          }]).then((response) => {
+            addDepartment(response).then(([rows]) => {
+              listAllDepartments().then(([rows]) => {
+                displayTable(rows);
+                console.log(`${response.departmentName} Department has been added to database.`)
+                start();
+              });
+            })
+          })
+      
     }
   })
 }
