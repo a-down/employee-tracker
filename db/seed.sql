0767198e513c336e86179254964be81e145e4d83
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-07 14:55:37 -0500

    updated seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -2,27 +2,27 @@ USE employees_db;
 
 INSERT INTO departments (department_name)
 VALUES
-    ('Sales'),
-    ('Engineering'),
-    ('Finance'),
-    ('Legal'),
-    ('Management');
+    ('Management'),
+    ('Kitchen'),
+    ('Financial'),
+    ('Quality Assurance'),
+    ('Facilities');
 
 INSERT INTO roles (role_title, salary, department_id)
 VALUES
-  ('Customer Rep', '80000', 1),
-  ('Senior Developer', '120000', 2),
-  ('Accountant', '75000', 3),
-  ('Legal Advisor', '130000', 4),
-  ('Site Manager', '90000', 5);
+  ('Store Manager', '80000', 1),
+  ('Fry Cook', '40000', 2),
+  ('Register', '30000', 3),
+  ('Product Tester', '20000', 4),
+  ('Maintenance Tech', '45000', 5);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
   VALUES
-	  ('Sandy', 'Cheeks', 5, null),
-    ('Patrick', 'Star', 1, 1),
+	  ('Eugene', 'Krabs', 1, null),
     ('Spongebob', 'Squarepants', 2, 1),
-    ('Eugene', 'Krabs', 3, 1),
-    ('Squidward', 'Tentacles', 4, 1);
+    ('Squidward', 'Tentacles', 3, 1),
+    ('Patrick', 'Stars', 4, 1),
+    ('Sandy', 'Cheeks', 5, 1);
   
 SELECT * FROM departments;
 SELECT * FROM roles;
